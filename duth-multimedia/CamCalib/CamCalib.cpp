5ccc7bf54347634ea0906eff10eaa56467ab7e58
cmavr8@gmail.com <cmavr8@gmail.com@89c5a362-8fa9-1b53-d4c2-0040d85a5f79>
2011-05-31 13:37:48 +0000

    Added GNU GPLv3 licence, deleted compiled files & XMLs.

--- duth-multimedia/CamCalib/CamCalib.cpp
+++ duth-multimedia/CamCalib/CamCalib.cpp
@@ -1,5 +1,13 @@
 // CamCalib.cpp : main project file.
-
+// Copyright 2011 Apostolos KOUTSOKALIS, Chris MAVRAKIS
+// 
+// This file is part of CamCalib.
+// 
+// CamCalib is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// 
+// CamCalib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with CamCalib. If not, see http://www.gnu.org/licenses/.
 #include "stdafx.h"
 #include "Form1.h"
 
